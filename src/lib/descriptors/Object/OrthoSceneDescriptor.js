77c09d462239b04b18a1847c9eff413419883987
bacti <tranmhaii@gmail.com>
2018-05-11 10:39:50 +0700

    +

---
+++ src/lib/descriptors/Object/OrthoSceneDescriptor.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import PropTypes from 'prop-types';
+
+import Object3DDescriptor from './Object3DDescriptor';
+import propTypeInstanceOf from '../../utils/propTypeInstanceOf';
+
+class SceneDescriptor extends Object3DDescriptor {
+  constructor(react3Instance) {
+    super(react3Instance);
+
+    this.hasProp('fog', {
+      type: PropTypes.oneOfType([
+        propTypeInstanceOf(THREE.Fog),
+        propTypeInstanceOf(THREE.FogExp2),
+      ]),
+      simple: true,
+      default: undefined,
+    });
+    this.hasProp('background', {
+      type: PropTypes.oneOfType([
+        propTypeInstanceOf(THREE.Color),
+        PropTypes.number,
+        PropTypes.string,
+      ]),
+      simple: true,
+      default: undefined,
+    });
+  }
+
+  applyInitialProps(threeObject: THREE.Scene, props) {
+    super.applyInitialProps(threeObject, props);
+    threeObject.background = new THREE.Color( props.background );
+  }
+
+  construct() {
+    return new THREE.Scene();
+  }
+}
+
+module.exports = SceneDescriptor;
